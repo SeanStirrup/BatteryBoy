686b2251975edf5c92dcfd1a1679fa80318f0a55
SeanStirrup <56080216+SeanStirrup@users.noreply.github.com>
2021-02-08 13:57:12 -0500

    Initial commit

---
+++ Assets/Scripts/DizzyBar.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//thanks to Casey Mfkn Urso for this bitchin script
+
+public class DizzyBar : MonoBehaviour
+{
+    public Slider DizzySlider;
+    public static float DizzyValue = 3000f;
+    public Animator animator;
+
+    private void Start()
+    {
+        animator.SetBool("IsDizzy", false);
+    }
+
+    void Update()
+    {
+        DizzySlider.value = DizzyValue;
+        DizzyValue--;
+
+        if (DizzyValue <= 0)
+        {
+            DizzyValue = 0f;
+            animator.SetBool("IsDizzy", true);
+            StartCoroutine(dizzyTime());
+        }
+    }
+
+    IEnumerator dizzyTime()
+    {
+        PhysicsMovement.runSpeed = 0f;
+        yield return new WaitForSeconds(4f);
+        PhysicsMovement.runSpeed = 10f;
+        DizzyValue = 3000f;
+        animator.SetBool("IsDizzy", false);
+    }
+}
---
+++ Assets/Scripts/PhysicsMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsMovement : MonoBehaviour
+{
+
+    public PlayerMovement controller;
+    public Animator animator;
+    float horizontalMove = 0f;
+    public  static float runSpeed = 10f;
+    bool jump = false;
+    
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        animator.SetFloat("speed", Mathf.Abs(horizontalMove));
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            animator.SetBool("IsJumping", true);
+        }
+    }
+
+    public void OnLanding()
+    {
+        jump = false;
+        animator.SetBool("IsJumping", false);
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        //jump = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            DizzyBar.DizzyValue = 3000f;
+            Destroy(other.gameObject);
+        }
+    }
+}
